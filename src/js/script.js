fd6b1d4d60b0f172864191b216937fa9af441a3a
Gabriel <Gabrielsilvarodrigues929@gmail.com>
2026-01-12 17:03:16 -0300

    Correções no codigo | estrutura condicional de valores.

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,55 @@
+//  Display
 const modal = document.getElementById("modal");
 const btnAbrir = document.getElementById("btnAbrir");
 const btnFechar = document.getElementById("btnFechar");
 
+//  Input
 const valorInput = document.getElementById("valorInput");
 const invalido = document.getElementById("invalido");
 
+//  Resultados armazenados em um banco de dados como array
+const tabela = [
+    { horas: 1, preco: 5 },
+    { horas: 2, preco: 10 },
+    { horas: 3, preco: 15 }
+];
+
+
 // Evento abrir modal
 btnAbrir.addEventListener("click", () => {
-    const tempo = Number(valorInput.value);
+    const dinheiro = Number(valorInput.value);
 
-    if (Number.isNaN(tempo) || tempo < 1) {
-        // mostra a mensagem de inválido
+    if (Number.isNaN(dinheiro) || dinheiro < 5) {
         invalido.style.display = "block";
-        return; // não abre o modal
+        return;
     }
 
-    // esconde a mensagem se estiver visível
     invalido.style.display = "none";
 
-    // abre o modal
-    modal.style.display = "block";
+    let horas, precoBase;
+
+    if (dinheiro < 10) {
+        horas = "1h";
+        precoBase = 5;
+    } else if (dinheiro < 15) {
+        horas = "2h";
+        precoBase = 10;
+    } else {
+        horas = "3h ou mais";
+        precoBase = 15;
+    }
+
+    const trocoCalculado = dinheiro - precoBase;
+
+    // atualiza os spans do modal
+    horario.textContent = horas;
+    valor.textContent = precoBase.toFixed(2).replace(".", ",");
+    troco.textContent = trocoCalculado.toFixed(2).replace(".", ",");
+
+    modal.style.display = "flex";
 });
+
+
 
 // Evento fechar modal
 btnFechar.addEventListener("click", () => {
